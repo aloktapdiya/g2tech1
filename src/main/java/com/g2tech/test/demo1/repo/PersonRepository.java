e3bc95e932a0cffb66ac43b3c11db5bd982a6426
alok.tapdiya <connect2alok@gmail.com>
2020-06-16 18:35:37 +0530

    added

---
+++ src/main/java/com/g2tech/test/demo1/repo/PersonRepository.java
@@ -0,0 +1,15 @@
+package com.g2tech.test.demo1.repo;
+
+import java.util.List;
+
+import com.g2tech.test.demo1.domain.Person;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
+
+    List<Person> findByLastName(@Param("name") String name);
+
+}
