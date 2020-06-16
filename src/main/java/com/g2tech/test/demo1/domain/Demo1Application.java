6895f954009678d5df0837e75fb1d4c946ed529f
alok.tapdiya <connect2alok@gmail.com>
2020-06-16 18:36:17 +0530

    added

--- src/main/java/com/g2tech/test/demo1/domain/Demo1Application.java
+++ src/main/java/com/g2tech/test/demo1/domain/Demo1Application.java
@@ -1,4 +1,4 @@
-package com.g2tech.test.demo1;
+package com.g2tech.test.demo1.domain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
