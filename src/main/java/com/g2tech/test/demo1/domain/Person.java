89190f251ecedb6c0c0a356d39bcc884a4ef9c9d
alok.tapdiya <connect2alok@gmail.com>
2020-06-17 12:23:56 +0530

    added

--- src/main/java/com/g2tech/test/demo1/domain/Person.java
+++ src/main/java/com/g2tech/test/demo1/domain/Person.java
@@ -1,19 +1,62 @@
 package com.g2tech.test.demo1.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+
+import javax.persistence.*;
+import java.io.Serializable;
 
 @Entity
-public class Person {
+@Table(name = "contactsapp")
+public class Person implements Serializable, Comparable<Person> {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-
+    @Column(name = "firstName")
     private String firstName;
+    @Column(name = "lastName")
+
     private String lastName;
+
+    private String personRole;
+
+    public String getAreacode() {
+        return areacode;
+    }
+
+    public void setAreacode(String areacode) {
+        this.areacode = areacode;
+    }
+
+    public String getPno() {
+        return pno;
+    }
+
+    public void setPno(String pno) {
+        this.pno = pno;
+    }
+
+    private String areacode;
+    private String pno;
+
+    public Person() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getPersonRole() {
+        return personRole;
+    }
+
+    public void setPersonRole(String personRole) {
+        this.personRole = personRole;
+    }
+
 
     public String getFirstName() {
         return firstName;
@@ -30,4 +73,31 @@ public class Person {
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+
+    public Person(String firstName, String areacode, String pno) {
+        super();
+        this.firstName = firstName;
+        this.areacode = areacode;
+        this.pno = pno;
+    }
+
+    public Person(Integer Id, String firstName, String areacode, String pno) {
+        super();
+        this.id = id;
+        this.firstName = firstName;
+        this.areacode = areacode;
+        this.pno = pno;
+    }
+
+    @Override
+    public String toString() {
+        return "Person [id=" + id + ", firstName=" + firstName + ", areacode=" + areacode + ", pno=" + pno + "]";
+    }
+
+    @Override
+    public int compareTo(Person c) {
+
+        return new Long(this.id).compareTo(c.getId());
+    }
+
 }
